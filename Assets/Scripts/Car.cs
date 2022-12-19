f880a64bf7adacbb2b76054537c1063c163bab2e
Kaech1997 <krzysztofherbot@gmail.com>
2022-12-19 15:39:41 +0100

    -upload project

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] float carSpeed = 1f;
+    [SerializeField] float carAcceleration = 2f;
+    [SerializeField] float carTurnSpeed = 10f;
+
+    private int steerValue;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMove();
+        ProcessTurn();
+    }
+
+    void ProcessMove()
+    {
+
+        carSpeed += carAcceleration * Time.deltaTime;
+        transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
+    }
+
+    void ProcessTurn()
+    {
+        transform.Rotate(0f, steerValue * carTurnSpeed * Time.deltaTime, 0);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
+    }
+
+}
